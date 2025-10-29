3606e0de4749086c4e10d2d748c9a9892a04e61b
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-10-29 19:53:02 -0400

    Filtro para a latitude e longitute

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -2,37 +2,38 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 function Starlink() {
-
   const [satelites, setSatelites] = useState([]);
 
   useEffect(() => {
-
     const fetchSatelites = async () => {
-      const response = await axios.post("https://api.spacexdata.com/v4/starlink/query", 
-      {
-        "query": {},
-        "options": { limit: 15 },
-      });
-      
-      setSatelites(response.data.docs)
-      console.log(response.data)
-    }
+      const response = await axios.post(
+        "https://api.spacexdata.com/v4/starlink/query",
+        {
+          query: {},
+          options: { limit: 100 },
+        }
+      );
+
+      setSatelites(response.data.docs);
+      console.log(response.data);
+    };
     fetchSatelites();
-    
-  },[])
+  }, []);
 
-
-  return( 
+  return (
     <>
       <h1>Lista de Satélites Starlink. Total: {satelites.length}</h1>
 
       <ul>
-        {satelites.map((sat) => (
-          <li key={sat.id}>{sat.spaceTrack.OBJECT_NAME} LAT {sat.latitude}  LONG{sat.longitude}</li>
-        )) }
+        {satelites
+          .filter((sat) => sat.latitude && sat.longitude)
+          .map((sat) => (
+            <li key={sat.id}>
+              {sat.spaceTrack.OBJECT_NAME} LAT {sat.latitude} LONG
+              {sat.longitude}
+            </li>
+          ))}
       </ul>
-
-      
     </>
   );
 }
